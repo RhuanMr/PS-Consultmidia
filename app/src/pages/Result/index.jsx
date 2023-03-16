c7a45a47cb4c7e29f9d97b270e658f59522259ef
rhuanm <magicknight@hotmail.com.br>
2023-03-16 19:14:41 -0300

    finish result page layout

--- app/src/pages/Result/index.jsx
+++ app/src/pages/Result/index.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import './styles.css';
 import ResultImage from './assets/resultImage.jpg'
 import BasicPage from '../../components/BasicPage';
+import ResultHooks from './hooks';
 
 const Result = () => {
+    const {
+        handleClick
+    } = ResultHooks();
     return(
         <BasicPage image={ResultImage}>
             <div className='containerResult'>
@@ -11,6 +15,7 @@ const Result = () => {
                 <h2>{`Sua viagem de ${48}km`}</h2>
                 <h2>foi aprovada no valor de</h2>
                 <span>{`R$ ${500}`}</span>
+                <button onClick={handleClick}>Aceitar</button>
             </div>
         </BasicPage>
     )
